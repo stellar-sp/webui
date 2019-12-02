05f22de1c9a6dcf2a538fa4cf0725a2b7e1c82cb
mlkbenjamin <mlkbenjamin94@gmail.com>
2019-12-02 17:05:03 +0330

    completing webui for create smart account

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Server, Network, Keypair, TransactionBuilder } from 'stellar-sdk';
+import { Server, Network, Keypair, TransactionBuilder, Operation } from 'stellar-sdk';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
@@ -13,12 +13,21 @@ export class AppComponent {
   title = 'webui';
   server = null;
   horizonAddress = 'https://horizon-testnet.stellar.org';
+  networkPassphrase = 'Test SDF Network ; September 2015';
   showDeployNewSmartProgram = true;
   showWorkersList = false;
   smartAccountPublicKey = null;
   smartAccountPrivateKey = null;
   smartAccountCreatorSeed = null;
+  smartAccountCreator = null;
+  emptyFileOnIpfsHash = "QmbFMke1KXqnYyBBWxB74N4c5SBnJMVAiMNRcGu6x1AwQH";
+
+  imageAddress = '';
+  imageHash = '';
+  executionFee = '1000';
+
   workers = [];
+  result_message = "";
   selected_workers = [
     {
       public_key: '',
@@ -31,18 +40,112 @@ export class AppComponent {
   onSubmit() {
     var self = this;
     (async function main() {
-      smartAccountCreatorKeyPair = KeyPair.fromSecret(self.smartAccountCreatorSeed)
 
-      var server = new Server(self.horizonAddress)
+      try {
 
-      smartAccountCreator = server.loadAccount(smartAccountCreatorKeyPair.address)
-      const fee = await server.fetchBaseFee()
-      new TransactionBuilder(smartAccountCreator, { fee })
+        self.result_message = new Date().toLocaleString() + " INFO: creating smart account..."
+
+        var smartAccountCreatorKeyPair = Keypair.fromSecret(self.smartAccountCreatorSeed)
+
+        self.server = new Server(self.horizonAddress)
+
+        self.smartAccountCreator = await self.server.loadAccount(smartAccountCreatorKeyPair.publicKey());
+
+        var lastestLedgerPromise = await self.http.get(self.horizonAddress + "/ledgers?order=desc&limit=1").toPromise()
+        var baseReserve = (lastestLedgerPromise._embedded.records[0].base_reserve_in_stroops / 10000000) * 2 + 10;
+
+        const fee = await self.server.fetchBaseFee()
+        const createSmartAccountTransaction = new TransactionBuilder(self.smartAccountCreator, { fee : fee, networkPassphrase : self.networkPassphrase })
+          .addOperation(
+            Operation.createAccount({
+              destination: self.smartAccountPublicKey,
+              startingBalance: baseReserve +""
+            })
+          )
+          .setTimeout(0)
+          .build();
+
+        createSmartAccountTransaction.sign(smartAccountCreatorKeyPair)
+        const createSmartAccountResult = await self.server.submitTransaction(createSmartAccountTransaction)
+
+        self.result_message += " done \n"
+
+        self.result_message += new Date().toLocaleString() + " INFO: setting smart program image specifications... "
+        var smartProgramSpecOps = [
+          Operation.manageData({ name: 'current_state', value: 'QmbFMke1KXqnYyBBWxB74N4c5SBnJMVAiMNRcGu6x1AwQH' }), // an empty file hash
+          Operation.manageData({ name: 'smart_program_image_address', value: self.imageAddress }),
+          Operation.manageData({ name: 'smart_program_image_hash', value: self.imageHash }),
+          Operation.manageData({ name: 'execution_fee', value: self.executionFee }),
+          Operation.manageData({ name: 'latest_transaction_changed_state', value: createSmartAccountResult.hash }),
+        ]
+
+        for (let i =0; i< self.selected_workers.length; i++) {
+          smartProgramSpecOps.push(Operation.manageData({ name: 'worker_'+ (i+1) +'_address', value: self.selected_workers[i].address }))
+          smartProgramSpecOps.push(Operation.manageData({ name: 'worker_'+ (i+1) +'_public_key', value: self.selected_workers[i].public_key }))
+          smartProgramSpecOps.push(Operation.setOptions({ signer : {
+              ed25519PublicKey: self.selected_workers[i].public_key,
+              weight: 1
+            }
+          }))
+        }
+
+        const percent51 = parseInt(self.selected_workers.length / 2) + 1
+        smartProgramSpecOps.push(Operation.setOptions({ masterWeight : 0, lowThreshold: percent51, medThreshold: percent51, highThreshold: percent51 }))
+
+        await self.signAndSubmit(self, smartProgramSpecOps)
+        self.result_message += "done \n"
+
+        self.result_message += "conguradulation. you created the smart account successfully";
+      }
+      catch(error) {
+        console.error(error);
+        self.result_message = error.message;
+      }
+
     })()
   }
 
+  async signAndSubmit(self, ops) {
+
+    const smartAccount = await self.loadAccountInfo(self, self.smartAccountPublicKey);
+
+    const fee = await self.getBaseFee(self)
+
+    var txBuilder = new TransactionBuilder(smartAccount, { fee : fee, networkPassphrase : self.networkPassphrase })
+      .setTimeout(0);
+
+    for (let op of ops) {
+      txBuilder.addOperation(op)
+    }
+
+    var tx = txBuilder.build()
+
+    var smartAccountKeyPair = Keypair.fromSecret(self.smartAccountPrivateKey)
+
+    tx.sign(smartAccountKeyPair)
+
+    var submitTxResult = await self.submitTx(self, tx)
+    console.log(submitTxResult)
+  }
+
+  async getBaseFee(self) {
+    var fee = await self.server.fetchBaseFee();
+    return fee;
+  }
+
+  async loadAccountInfo(self, accountPublicKey) {
+    var account = await self.server.loadAccount(accountPublicKey)
+    return account
+  }
+
+  async submitTx(self, tx) {
+    const xdr = tx.toEnvelope().toXDR().toString("base64")
+    console.log(xdr);
+    var res = await self.server.submitTransaction(tx);
+    return res;
+  }
+
   generateNewKeyPair() {
-    this.server = new Server('https://horizon.stellar.org');
     var keypair = Keypair.random();
     this.smartAccountPublicKey = keypair.publicKey();
     this.smartAccountPrivateKey = keypair.secret();
